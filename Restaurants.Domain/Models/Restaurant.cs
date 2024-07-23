9ca7c5e166ebabc3067a52a8bafb565160431175
Tshindimpho <mudzu13@gmail.com>
2024-07-23 22:54:48 +0200

    Add project files.

---
+++ Restaurants.Domain/Models/Restaurant.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace Restaurants.Domain.Models
+{
+    public class Restaurant
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } 
+        public string Description { get; set; } 
+        public string Category { get; set; } 
+        public bool HasDelivery { get; set; } 
+        public string ContactEmail { get; set; } 
+        public string ContactNumber { get; set; } 
+        public Address Address { get; set; }
+        public List<Dish> Dishes { get; set; } = new();
+    }
+}
