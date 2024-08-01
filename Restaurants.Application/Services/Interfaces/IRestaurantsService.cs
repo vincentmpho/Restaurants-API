d5ddfe0f189ac4c5b7f438fc7814b5da345b9f29
Tshindimpho <mudzu13@gmail.com>
2024-08-01 21:31:23 +0200

    Add the  Restaurants Services

---
+++ Restaurants.Application/Services/Interfaces/IRestaurantsService.cs
@@ -0,0 +1,21 @@
+﻿using Restaurants.Application.DTOs;
+using Restaurants.Domain.Models;
+
+namespace Restaurants.Application.Services.Interfaces
+{
+    public interface IRestaurantsService
+
+    {/// <summary>
+     /// Retrieves a list of all restaurants.
+     /// </summary>
+     /// <returns>returns IEnumerable of Restaurant objects representing all restaurants.</returns>
+     Task<IEnumerable<RestaurantDto>> GetAllRestaurants();
+
+        /// <summary>
+        /// Retrieves a restaurant by its unique identifier.
+        /// </summary>
+        /// <param name="id">The unique identifier of the restaurant.</param>
+        /// <returns>Returns a Restaurant object representing the restaurant with the specified ID.</returns>
+        Task<RestaurantDto> GetById(Guid id);
+    }
+}
---
+++ Restaurants.Application/Services/RestaurantsService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Logging;
+using Restaurants.Application.DTOs;
+using Restaurants.Application.Services.Interfaces;
+using Restaurants.Domain.Interfaces.Repositories;
+using Restaurants.Domain.Models;
+
+namespace Restaurants.Application.Services
+{
+    public class RestaurantsService : IRestaurantsService
+    {
+        private readonly IRestaurantRepository _restaurantRepository;
+        private readonly ILogger<RestaurantsService> _logger;
+
+        public RestaurantsService(IRestaurantRepository restaurantRepository, ILogger<RestaurantsService> logger)
+        {
+            _restaurantRepository = restaurantRepository;
+            _logger = logger;
+        }
+        public async Task<IEnumerable<RestaurantDto>> GetAllRestaurants()
+        {
+            _logger.LogInformation("Getting all restaurants");
+            var restaurants = await _restaurantRepository.GetAllAsync();
+
+            var rstaurantsDto = restaurants.Select(r => new RestaurantDto() 
+            { 
+              Category = r.Category,
+              Description = r.Description,
+              Id = r.Id,
+              HasDelivery = r.HasDelivery,
+              Name = r.Name,
+              City = r.Address.City,
+              Street= r.Address.Street,
+              PostalCode= r.Address.PostalCode,
+            });
+            return rstaurantsDto;
+        }
+
+        public async Task<RestaurantDto> GetById(Guid id)
+        {
+            _logger.LogInformation($"Getting Restaurants by ID {id}");
+            var restaurants = await _restaurantRepository.GeyByIdAsync(id);
+            return restaurants;
+        }
+    }
+}
