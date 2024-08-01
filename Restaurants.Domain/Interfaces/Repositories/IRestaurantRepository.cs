5b0f3d61510c0e90c4db4e0e7915e18e06e897e0
Tshindimpho <mudzu13@gmail.com>
2024-08-01 21:32:01 +0200

    Add Repository Interface for Restaurants

---
+++ Restaurants.Domain/Interfaces/Repositories/IRestaurantRepository.cs
@@ -0,0 +1,10 @@
+﻿using Restaurants.Domain.Models;
+
+namespace Restaurants.Domain.Interfaces.Repositories
+{
+    public interface IRestaurantRepository
+    {
+        Task<IEnumerable< Restaurant>> GetAllAsync();
+        Task<Restaurant> GeyByIdAsync(Guid id);
+    }
+}
