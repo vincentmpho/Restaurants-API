9f7ff4f1700691177333edc8b53a1ddfb9cfb2a9
Tshindimpho <mudzu13@gmail.com>
2024-08-02 13:18:16 +0200

    Implement the DishDto and RestaurantDtos

---
+++ Restaurants.Application/DTOs/DishDto.cs
@@ -0,0 +1,25 @@
+﻿using Restaurants.Domain.Models;
+
+namespace Restaurants.Application.DTOs
+{
+    public class DishDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Decimal Price { get; set; }
+        public int KiloCaloriess { get; set; }
+        
+        public static DishDto FromEntity(Dish dish)
+        {
+            return new DishDto()
+            {
+                Id = dish.Id,
+                Name = dish.Name,
+                Description = dish.Description,
+                Price = dish.Price,
+                KiloCaloriess = dish.KiloCaloriess
+            };
+        }
+    }
+}
---
+++ Restaurants.Application/DTOs/RestaurantDto.cs
@@ -0,0 +1,38 @@
+﻿using Restaurants.Domain.Models;
+
+namespace Restaurants.Application.DTOs
+{
+    public class RestaurantDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool HasDelivery { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; } 
+        public List<DishDto> Dishes { get; set; } = []; 
+
+
+        public static RestaurantDto? FromEntity(Restaurant? restaurant)
+        {
+
+            if (restaurant == null) return null;
+
+            return new RestaurantDto()
+
+            {
+                Category = restaurant.Category,
+                Description = restaurant.Description,
+                Id = restaurant.Id,
+                HasDelivery = restaurant.HasDelivery,
+                Name = restaurant.Name,
+                City = restaurant.Address.City,
+                Street = restaurant.Address.Street,
+                PostalCode = restaurant.Address.PostalCode,
+                Dishes= restaurant.Dishes.Select( DishDto.FromEntity).ToList()
+            };
+        }
+    }
+}
