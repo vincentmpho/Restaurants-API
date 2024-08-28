24457f6dd991cf0fc2489e460e350dbcbfb4e6d1
Tshindimpho <mudzu13@gmail.com>
2024-08-28 14:56:55 +0200

    Implement unit test for UserContext Tests

---
+++ Restaurants.ApplicationTests/Users/CurrentUserTests.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using Restaurants.Domain.Contants;
+using Xunit;
+
+namespace Restaurants.Application.Users.Tests
+{
+    public class CurrentUserTests
+    {
+        //TestMethod_Scenario_ExpectResult
+        [Theory()]
+        [InlineData(UserRoles.Admin)]
+        [InlineData(UserRoles.User)]
+        public void IsInRole_withMatchingRole_shouldReturnTrue(string roleName)
+        {
+            //arrage
+            var currentUser = new CurrentUser("1","test@test.com", [UserRoles.Admin,UserRoles.User]);
+
+
+            //act
+
+           var isInRole =  currentUser.IsInRole(roleName);
+
+            //assert/result
+
+            isInRole.Should().BeTrue();
+        }
+
+        //TestMethod_Scenario_ExpectResult
+        [Fact()]
+        public void IsInRole_withNoMatchingRole_shouldReturnFalse()
+        {
+            //arrage
+            var currentUser = new CurrentUser("1", "test@test.com", [UserRoles.Admin, UserRoles.User]);
+
+
+            //act
+
+            var isInRole = currentUser.IsInRole(UserRoles.Owner);
+
+            //assert/result
+
+            isInRole.Should().BeFalse();
+        }
+
+        //TestMethod_Scenario_ExpectResult
+        [Fact()]
+        public void IsInRole_withNoMatchingRoleCase_shouldReturnFalse()
+        {
+            //arrage
+            var currentUser = new CurrentUser("1", "test@test.com", [UserRoles.Admin, UserRoles.User]);
+
+
+            //act
+
+            var isInRole = currentUser.IsInRole(UserRoles.Admin.ToLower());
+
+            //assert/result
+
+            isInRole.Should().BeFalse();
+        }
+    }
+}
