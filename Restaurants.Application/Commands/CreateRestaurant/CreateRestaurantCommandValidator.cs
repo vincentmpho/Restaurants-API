8ce8cb4f6f8597ae1f2b8e46b0f1b62c4efc82f9
Tshindimpho <mudzu13@gmail.com>
2024-08-29 12:06:46 +0200

    Add PostalCode validation on the CreateRestaurantCommandValidator

--- Restaurants.Application/Commands/CreateRestaurant/CreateRestaurantCommandValidator.cs
+++ Restaurants.Application/Commands/CreateRestaurant/CreateRestaurantCommandValidator.cs
@@ -18,6 +18,9 @@ namespace Restaurants.Application.Commands.CreateRestaurant
                 .EmailAddress()
                 .WithMessage("Please provide  a valid email address");
 
+            RuleFor(dto => dto.PostalCode)
+                .Matches(@"\d{2}-\d{3}$")
+                .WithMessage("please provide a valid postal code (XX-XXX).");
         }
     }
 }
